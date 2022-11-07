25fab15a7b9e318cf146c70ba16f12c175c2256b
Ivan Alvarez <alvarezivan001@yahoo.com>
2022-11-07 13:48:46 -0800

    Add divide by zero catch, and mulitple digit numbs

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let numberButtons = document.querySelectorAll('button');
 let displayValue = 0;
 let operator = '';
 let previousValue = 0;
+let giveAnswer = false;
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
         if(typeof Number(button.getAttribute('id')) == 'number' && !isNaN(Number(button.getAttribute('id'))) )
-       { display(button.getAttribute('id'));}
+       { display(button.getAttribute('id'), giveAnswer);}
         else if(button.getAttribute('class') == "op")
         {
             operator = button.getAttribute('id');
@@ -16,7 +17,13 @@ numberButtons.forEach((button) => {
         else if(button.getAttribute('id') == '=')
         {
             let operation = operate(previousValue,operator,displayValue);
-            display(operation.toString());
+            giveAnswer = true;
+            if(isNaN(operation)) {
+                alert("Do not divide by 0 plz");
+                clearCalc();
+                giveAnswer = false;
+            }
+            else{display(operation, giveAnswer);}
             
         }
         else if(button.getAttribute('id') == 'clear')
@@ -36,21 +43,29 @@ const divide = function(a, b){
     return a/b;
 }
 const operate = function(a, op, b){
-    return op == '+' ? add(a,b) :
-            op == '-' ? subtract(a,b) :
-            op == '*' ? multiply(a,b) :
-            op == '/' ? divide(a,b) :
+    if(op == '/' && b == 0)
+    {return 'OOPS';}
+
+    return op == '+' ? add(a,b).toString() :
+            op == '-' ? subtract(a,b).toString() :
+            op == '*' ? multiply(a,b).toString() :
+            op == '/' ? divide(a,b).toString() :
             'OOPS';
 }
-const display = function(buttonId) {
+const display = function(buttonId, answer) {
     const divDisplay = document.querySelector('div');
-    if(displayValue == 0)
+    if(displayValue == 0 && !answer)
     {displayValue = Number(buttonId);}
-    else
+    else if(!answer)
     {
         let concatValue = displayValue.toString();
         concatValue += buttonId;
         displayValue = Number(concatValue);
+    }
+    else
+    {
+        displayValue = Number(buttonId);
+        giveAnswer = false;    
     }
     divDisplay.textContent = displayValue;
     // numberButtons = document.querySelectorAll('button');
@@ -79,5 +94,6 @@ const clearCalc = function() {
     displayValue = 0;
     operator = '';
     previousValue = 0;
-    display(displayValue.toString());
+    giveAnswer = false;
+    display(displayValue.toString(), giveAnswer);
 }
